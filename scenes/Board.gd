c3a04266f2354cea0ce51fe0b39573f3dfaf0904
MatiasNegro <67411732+MatiasNegro@users.noreply.github.com>
2022-01-10 15:07:15 +0100

    Third_commit

--- scenes/Board.gd
+++ scenes/Board.gd
@@ -2,14 +2,21 @@ extends Node2D
 
 
 var board
-
+var Tile = preload("res://scenes/Tile.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#board = get_tree().get_nodes_in_group()
-	pass
+	board = get_tales()
+	print(board)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func get_tales():
+	var temp = []
+	for i in self.get_children():
+		if i is Tile:
+			temp.append(i)
+
+	return temp
