632220765592affd96877058148c982ed3efe1b2
MatiasNegro <67411732+MatiasNegro@users.noreply.github.com>
2022-01-20 21:48:15 +0100

    cells_working

--- scenes/Board.gd
+++ scenes/Board.gd
@@ -2,45 +2,40 @@ extends Node2D
 class_name Board
 var SIZE = 8
 var tiles = []
+var tile_source = preload("res://scenes/Tile.tscn")
 var new_tile 
 var first_tile
 var init_pos = {
-	pos_x = 115,
-	pos_y = 119,
+	pos_x = 110,
+	pos_y = 115,
 }
 
-var incr_pos = {
+var to_incr = {
 	x = 57,
-	y = 56,
+	y = 57,
 }
-var incr_x = 1
-var incr_y = 1
+var incr_x = 0
+var incr_y = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Sprite.show()
+	#$Sprite.hide()
 	generate_tiles()
-	for i in range(SIZE * SIZE):
-		tiles[i].show()
+	print(self.get_children())
+#	for i in range(SIZE * SIZE):
+#		tiles[i].show()
 		
 	
 func generate_tiles():
-	first_tile = load("res://scenes/Tile.tscn").instance()
-	first_tile.set_position(Vector2(init_pos.pos_x , init_pos.pos_y))
-	add_child(first_tile)
-	tiles.append(first_tile)
-	for i in range(SIZE * SIZE):
-			new_tile = load("res://scenes/Tile.tscn").instance()
-			if i % 8 != 0:
-				new_tile.set_position(Vector2(init_pos.pos_x + (incr_x * incr_pos.x), init_pos.pos_y))
-				incr_x +=1
-			else:
-				new_tile.set_position(Vector2(init_pos.pos_x , init_pos.pos_y + (incr_y * incr_pos.y)))
-				incr_x = 1
-				incr_y += 1
-			add_child(new_tile)
-			tiles.append(new_tile)
-			
-			
-
+	for i in range(1, (SIZE * SIZE) + 8 ):
+		if i % 9 != 0:
+			new_tile = tile_source.instance()
+			new_tile.set_position(Vector2(init_pos.pos_x + (incr_x * to_incr.x), init_pos.pos_y + (incr_y * to_incr.y)))
+			incr_x +=1
+		else:
+			incr_x = 0
+			incr_y += 1
+		self.add_child(new_tile)
+		#tiles.append(new_tile)
 
 
--- scenes/Tile.gd
+++ scenes/Tile.gd
@@ -4,30 +4,21 @@ class_name Tile
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var id
-var pos_x = 0
-var pos_y = 0
-
-func _init(new_id):
-	self.id = new_id
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#$Sprite.hide()
-	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	pass
-
+	$Sprite.hide()
 
 func _on_Tile_mouse_entered():
-	#$Sprite.show()
-	pass
+	
+	$Sprite.show()
+	
 
 
 func _on_Tile_mouse_exited():
-	#$Sprite.hide()
-	pass
+	$Sprite.hide()
+	
 
 func set_position(new_position: Vector2):
 	position = new_position
+
+
