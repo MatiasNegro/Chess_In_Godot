1db3619b38fb7476083e55827005c24003204afb
MatiasNegro <67411732+MatiasNegro@users.noreply.github.com>
2022-01-31 11:29:52 +0100

    added_print_black_pawns

---
+++ scenes/pieces/black_pawn.gd
@@ -0,0 +1,12 @@
+extends "res://scripts/Piece.gd"
+
+
+
+func init(rest_zone):
+	set_rest(rest_zone)
+	
+func set_where_rested_id(tile):
+	where_rested_id = tile.get_id()
+
+	
+
--- scripts/Board.gd
+++ scripts/Board.gd
@@ -2,10 +2,22 @@ extends Node2D
 
 var SIZE = 8
 var tile = load("res://scenes/Tile.tscn")
-var piece = load("res://scenes/black_horse.tscn")
-var wh = load("res://scenes/white_horse.tscn")
+var bh = load("res://scenes/pieces/black_horse.tscn")
+var wh = load("res://scenes/pieces/white_horse.tscn")
+var bq = load("res://scenes/pieces/black_queen.tscn")
+var wq = load("res://scenes/pieces/white_queen.tscn")
+var bk = load("res://scenes/pieces/black_king.tscn")
+var wk = load("res://scenes/pieces/white_king.tscn")
+var bb = load("res://scenes/pieces/black_bishop.tscn")
+var wb = load("res://scenes/pieces/white_bishop.tscn")
+var br = load("res://scenes/pieces/black_rook.tscn")
+var wr = load("res://scenes/pieces/white_rook.tscn")
+var bp = load("res://scenes/pieces/black_pawn.tscn")
+var wp = load("res://scenes/pieces/white_pawn.tscn")
+#Standard for the array => [pawn, rook, horse, bishop, king, queen]
+var white_pieces = [wp, wr, wh, wb, wk, wq] 
+var black_pieces = [bp, br, bh, bb, bk, bq] 
 var move_log = []
-#onready var move = get_node("Move")
 
 var pos = {
 	x = 0,
@@ -18,10 +30,7 @@ var to_incr_y = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_tiles()
-	var new_piece = piece.instance()
-	add_child(new_piece)
-	var new_wh = wh.instance()
-	add_child(new_wh)
+	load_pieces()
 
 
 func set_tiles():
@@ -37,3 +46,15 @@ func set_tiles():
 				continue
 			to_incr_y += 1
 			to_incr_x = 0
+			
+func load_pieces():
+	var tiles = get_tree().get_nodes_in_group("tile")
+	var new_piece
+	#load of the black pawns
+	for tile in tiles:
+		if(tile.get_id() / 10 == 2):
+			new_piece = black_pieces[0].instance()
+			new_piece.init(tile.get_node("drop_zone").get_global_position())
+			add_child(new_piece)
+			new_piece.set_where_rested_id(tile)
+	
